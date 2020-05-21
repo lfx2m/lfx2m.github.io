769578bd29533cc69f569230282455f877743108
Stefan <stefan.kubisa@campus.tu-berlin.de>
2020-05-22 00:53:40 +0200

    2nd iteration

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const header = new Vue({
     el: "#header",
     template: `
-    <header class="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar">
+    <header class="navbar col-12 navbar-expand navbar-dark flex-column flex-md-row bd-navbar bg-dark text-white">
         <div class="navbar-nav-scroll">
             <ul class="navbar-nav bd-navbar-nav flex-row">
                 <li class="nav-item">
@@ -29,7 +29,6 @@ const header = new Vue({
                         onclick="ga('send', 'event', 'Navbar', 'Community links', 'Blog');" target="_blank"
                         rel="noopener">Instagram</a>
                 </li>
-                
             </ul>
         </div>
     </header>
@@ -39,12 +38,21 @@ const header = new Vue({
 const mainer = new Vue({
     el: "#mainer",
     template: `
-        
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <img class="img rounded-circle align-self-center" width="240" 
+                src="Images/StefanKubisa.jpeg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"
+                aria-label="Placeholder: 140x140">
+                <title>Placeholder</title>
+            </img>
+            <h2>Heading</h2>
+            <p>I study Industrial Economics with focus on Information and Communication Technologies at the Technical                    University of Berlin and enjoy both reading and writing of academic papers as well as Capture the Falg competitions.</p>
+        </div>
+    </div>
     `
 })
 const footer = new Vue({
     el: "#footer",
-    template: `
-        <h1>ddd + {{ testingCode }} + </h1>                
+    template: `      
     `
 })

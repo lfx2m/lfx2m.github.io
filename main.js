16139afc3a8411d6c1d4df0ebc6e2285024d539f
stefankubisa <lfx2m@o2.pl>
2021-06-04 22:18:31 +0200

    Collab Time

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-var card = new Vue({
-    el: '#card',
+CardObject = new Vue({
+    el: '#cards',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue.js' },
+            { text: 'Build Something Awesome' }
+        ],
+        locations: [
+            { location: './Images/Tropical Morning.png'},
+            { location: './Images/Leaf Of Faith.png'},
+            { location: './Images/Gryfia Stocznia Remontowa.png'}
+        ]
+    }
+})
+
+new Vue({
+    el: "#app",
+    data: {
+        image: './Images/Leaf Of Faith.png',
+        image: './Images/Tropical Morning.png'
+    },
+})
+
+test = new Vue({
+    el: '#test',
     template: `
-        <img src="./Images/Badger The Alchemist.png" alt="">
-    `
+        <h2>{{ image }}</h2>
+        <img v-for="z in image" v-bind:src="image" alt="">
+    `,
+    data: {
+        image: './Images/Leaf Of Faith.png',
+        image: './Images/Tropical Morning.png'
+    }
 })
